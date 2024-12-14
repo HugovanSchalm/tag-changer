f19f631e1a0abb7e2eb31d5fc1f69e407d2e0f23
Hugo van Schalm <hugovanschalm@hotmail.com>
2024-12-14 23:49:25 +0100

    Converted command line structure to multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, Command};
+use clap::{Arg, Command, Subcommand};
 use std::{fs::File, path::PathBuf};
 
 use tag_changer::{ID3v1, ReadError};
@@ -7,23 +7,46 @@ fn main() {
     let matches = Command::new("tag-changer")
         .author("Hugo van Schalm")
         .version("0.1.0")
-        .arg(
-            Arg::new("file")
-                .required(true)
-                .value_parser(clap::builder::StringValueParser::new()),
+        .subcommand(Command::new("view")
+            .short_flag('v') 
+            .long_flag("view")
+            .arg(
+                Arg::new("file")
+                    .required(true)    
+                    .value_parser(clap::builder::StringValueParser::new()),
+            )
+        )
+        .subcommand(Command::new("edit")
+            .short_flag('e') 
+            .long_flag("edit")
+            .arg(
+                Arg::new("file")
+                    .required(true)    
+                    .value_parser(clap::builder::StringValueParser::new()),
+            )
+            .arg(
+                Arg::new("name")
+                    .short('n')
+                    .long("name")
+                    .value_parser(clap::builder::StringValueParser::new()),
+            )
         )
         .get_matches();
+    match matches.subcommand(){
+        Some(("view", viewmatches)) => {
+            let filestring: &String = viewmatches.get_one("file").unwrap();
+            let filepath = PathBuf::from(filestring);
 
-    let filestring: &String = matches.get_one("file").unwrap();
-    let filepath = PathBuf::from(filestring);
+            let mut file = File::open(filepath).unwrap();
 
-    let mut file = File::open(filepath).unwrap();
+            let tags = match ID3v1::read(&mut file) {
+                Ok(tags) => tags,
+                Err(ReadError::ID3) => panic!("Could not parse tags of file {}", filestring),
+                Err(ReadError::IO(err)) => panic!("Could not open file: {}", err),
+            };
 
-    let tags = match ID3v1::read(&mut file) {
-        Ok(tags) => tags,
-        Err(ReadError::ID3) => panic!("Could not parse tags of file {}", filestring),
-        Err(ReadError::IO(err)) => panic!("Could not open file: {}", err),
-    };
-
-    println!("{}", tags);
+            println!("{}", tags);
+        }
+        _ => unreachable!("This subcommand should not exist"),
+    }  
 }
